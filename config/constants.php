19474a597b082fe9d4e037e9726d06301dff3266
Bennadji Abdelali <138626004+Bendz07@users.noreply.github.com>
2026-08-14 02:02:18 +0100

    feat: add application constants

---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+const ROLE_ADMIN = 'admin';
+const ROLE_OFFICER = 'officer';
+const ROLE_VOTER = 'voter';
+
+const ELECTION_DRAFT = 'draft';
+const ELECTION_SCHEDULED = 'scheduled';
+const ELECTION_ACTIVE = 'active';
+const ELECTION_CLOSED = 'closed';
+const ELECTION_ARCHIVED = 'archived';
